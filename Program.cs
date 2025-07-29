91dceb0227e5a2ef5670fc776b230586750f741b
eteixeira <teixeira.abdelhack@gmail.com>
2025-07-28 21:58:11 -0300

    Versão 1.0
    Original

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+int n1;
+
+Console.Write("Informe um numero para obter seu modulo: ");
+n1 = int.Parse(Console.ReadLine());
+
+Console.Clear();
+if (n1 >= 0)
+{
+    Console.WriteLine($"Modulo de {n1} = {n1}");
+}
+else
+{
+    Console.WriteLine($"Modulo de {n1} = {n1 * (-1)}");
+}
+
